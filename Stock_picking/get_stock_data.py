ac109443431447c69198f53232b3245fe040418e
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-05-30 09:30:48 -0700

    adding dates and comments on how to set the dates manually

--- Stock_picking/get_stock_data.py
+++ Stock_picking/get_stock_data.py
@@ -3,13 +3,14 @@ import yfinance as yf
 import yahoofinancials
 import datetime 
 
+#input the stock ticker which will then download the data from yahoo finance 
 print("please input the stock that you are interested in")
 stock = input()
 
 
-time_min="5m"
-days="1d"
+#we use yahoo finance to specify the dates 
+stock_df = yf.download(stock, start='2020-01-01',end='2020-05-22')
 
-stock_df = yf.download(stock, interval=time_min,period=days)
 
+#this will spit out a data frame with open,close,adj close, volume
 print(stock_df.head())
