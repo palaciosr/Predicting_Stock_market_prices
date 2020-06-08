431020bb447715dd4a127924a7663d44ddf13bd0
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-08 15:47:13 -0700

    changed the app to deal with less dependencies for now

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask 
 import os, sys 
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+# sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-from Stock_averages.stock_averages import StockAverage
+# from Stock_averages.stock_averages import StockAverage
 
 app = Flask(__name__)
 
@@ -11,9 +11,9 @@ app = Flask(__name__)
 def price():
 
     #sp 500 average and statistics 
-    stock_avg,stock_stats= StockAverage().get_stock_average()
+    # stock_avg,stock_stats= StockAverage().get_stock_average()
 
-    print(type(stock_avg))
+    # print(type(stock_avg))
 
     return "the prices"
 
