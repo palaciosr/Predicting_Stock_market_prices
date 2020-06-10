9687e857a6d8189af9677c2f1790d4bb37a9cef1
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-10 15:28:10 -0700

    checking the Flask app can run the random forest algo

--- app.py
+++ app.py
@@ -1,21 +1,34 @@
 from flask import Flask 
 import os, sys 
 
-# sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 # from Stock_averages.stock_averages import StockAverage
 
+from Stock_predictions.random_forest_prediction import MLPredictions
+
 app = Flask(__name__)
 
-@app.route('/')
-def price():
 
-    #sp 500 average and statistics 
-    # stock_avg,stock_stats= StockAverage().get_stock_average()
+
+"""
+the prediction gives a 98% score we should be wary of this 
+It is important to note that this is a backtest as opposed to a forward test 
+the only way to make money is to forward test I will conduct further research
+
+"""
+
+@app.route('/')
+def prediction_power():
+
+    ml_pred = MLPredictions()
+    price_pred = ml_pred.random_forest()
+
+
 
     # print(type(stock_avg))
 
-    return "the prices"
+    return price_pred
 
 
 if __name__ == '__main__':
