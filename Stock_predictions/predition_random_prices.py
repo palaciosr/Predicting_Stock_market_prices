16f7da9935e3c2f178391f8f488ec703af84c517
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-11 14:54:13 -0700

    Pushing for random prices in 6 months and for the past 2 weeks. Also including actual prices for the week of June 8, 2020

--- Stock_predictions/predition_random_prices.py
+++ Stock_predictions/predition_random_prices.py
@@ -1,14 +1,55 @@
 import random
 import pandas_datareader as web
 
-#we get a data with date and prices open close adjusted volume 
-data = web.DataReader('^GSPC',data_source='yahoo',start='2020-01-05',end='2020-06-05')
-
-min_price = data['Open'].min()
-max_price = data['Open'].max()
-
 #predict for the next five days 
 #week of June 8, 2020
-for sp500_price in range(5):
+#this comes from the last 6 months 
+def last_six_months(min_price_6_months,max_price_6_months):
 
-    print(random.randrange(int(min_price), int(max_price))) 
+    for sp500_price in range(4):
+
+        print(random.randrange(int(min_price_6_months), int(max_price_6_months))) 
+
+
+def last_2wks(min_price_2wks,max_price_2wks):
+
+    for sp500_price in range(4):
+       
+        print(random.randrange(int(min_price_2wks), int(max_price_2wks))) 
+
+def actual_prices():
+
+    data = web.DataReader('^GSPC',data_source='yahoo',start='2020-06-08',end='2020-06-11')
+
+    print(data['Open'])
+
+def main():
+
+    #we get a data with date and prices open close adjusted volume 
+    data_6mo = web.DataReader('^GSPC',data_source='yahoo',start='2020-01-05',end='2020-06-05')
+
+    min_price_6_months = data_6mo['Open'].min()
+    max_price_6_months = data_6mo['Open'].max()
+
+    #lets get random prices for the next 5 days given the last 6 months
+    print("Prices given the last 6 months randomly")
+    last_six_months(min_price_6_months,max_price_6_months)
+    print()
+
+    data_2wks = web.DataReader('^GSPC',data_source='yahoo',start='2020-05-03',end='2020-06-05')
+
+    min_price_2wks = data_2wks['Open'].min()
+    max_price_2wks = data_2wks['Open'].max()
+
+    print("Prices given the last 2 weeks randomly")
+    last_six_months(min_price_2wks,max_price_2wks)
+    print()
+
+    print("The actual prices for the prices for the week of June 8, 2020 ")
+    actual_prices()
+
+main()
+
+
+
+    
