9c62d59afa29d33af9f062464bdc5dd940d1e61c
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-11 18:50:04 -0700

    adding the random predictions generated prices

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import os, sys
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-# from Stock_averages.stock_averages import StockAverage
 
 from Stock_predictions.random_forest_prediction import MLPredictions
+
+from Stock_predictions.predition_random_prices import main 
 
 app = Flask(__name__)
 
@@ -21,14 +22,15 @@ the only way to make money is to forward test I will conduct further research
 @app.route('/')
 def prediction_power():
 
-    ml_pred = MLPredictions()
-    price_pred = ml_pred.random_forest()
+    # ml_pred = MLPredictions()
+    # price_pred = ml_pred.random_forest()
+    x,y,z =main()
 
 
 
-    # print(type(stock_avg))
+    print("The prices are ")
 
-    return price_pred
+    return x,y,z 
 
 
 if __name__ == '__main__':
