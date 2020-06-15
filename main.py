fa24662902d4ee47f5c3a380b1fdc7e2db574301
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-15 15:38:41 -0700

    This python file gives a feel for how different methods of prediciting stock markets work. We do indeed see that deep learning does best although still not accurate

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from Stock_predictions.predition_random_prices import all_pred
 from Stock_predictions.ml_sp500 import StockData
 
 
+stock_data= StockData()
+
+date=stock_data.get_pred_dates()
+
+print("dates and prices from the deep learning model")
+print(date)
+print()
+#below will give the future prices and dates 
+
+
+list_prices=stock_data.predict_prices()
+print(list_prices)
 
 
 all_pred()
