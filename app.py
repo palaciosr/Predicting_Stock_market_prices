4b22f47528f41036deecbd0e193abb41361c40fd
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-17 16:14:45 -0700

    adding logic to get stock data

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 
 from Stock_predictions.random_forest_prediction import MLPredictions
-
 from Stock_predictions.predition_random_prices import last_six_months 
+from Stock_picking import get_stock_data
 
 app = Flask(__name__)
 
@@ -26,21 +26,40 @@ the only way to make money is to forward test I will conduct further research
 """
 
 @app.route('/')
-def prediction_power():
+def main():
+    return redirect('home.html')
 
-    # ml_pred = MLPredictions()
-    # price_pred = ml_pred.random_forest()
-    x =last_six_months()
 
-    print("The prices predictions are ")
+@app.route('/home.html',methods=['GET'])
+def home():
 
     return render_template('home.html')
 
+
 @app.route('/graph')
 def graph():
+
+    p = figure(title='Stock prices displayed  for %s' %)
 
 
 
 if __name__ == '__main__':
 
     app.run()
+
+
+
+"""
+#will come last but will  display multiple predictions
+
+# @app.route('/')
+# def prediction_power():
+
+#     # ml_pred = MLPredictions()
+#     # price_pred = ml_pred.random_forest()
+#     x =last_six_months()
+
+#     print("The prices predictions are ")
+
+#     return render_template('home.html')
+"""
