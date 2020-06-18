1913a7c3a06ea35e9a4a2f8ec77d1d065222b35f
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-18 13:58:00 -0700

    uncommenting the print

--- Stock_picking/get_stock_data.py
+++ Stock_picking/get_stock_data.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import yfinance as yf 
-import yahoofinancials
+# import yahoofinancials
 import datetime 
 
 #input the stock ticker which will then download the data from yahoo finance 
@@ -13,4 +13,4 @@ stock_df = yf.download(stock, start='2020-01-01',end='2020-05-22')
 
 
 #this will spit out a data frame with open,close,adj close, volume
-print(stock_df.head())
+# print(stock_df.head())
