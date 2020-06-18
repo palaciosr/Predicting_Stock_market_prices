cb412484c16a524dcfbdd0e50bb2b85c327ef253
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-18 13:58:51 -0700

    adding GET functionality to flask

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 from Stock_predictions.random_forest_prediction import MLPredictions
 from Stock_predictions.predition_random_prices import last_six_months 
-from Stock_picking import get_stock_data
+# from Stock_picking import get_stock_data
 
 app = Flask(__name__)
 
@@ -29,6 +29,10 @@ the only way to make money is to forward test I will conduct further research
 def main():
     return redirect('home.html')
 
+#api key:
+
+#pSwkUPQr8XpJf4xeFM7S
+
 
 @app.route('/home.html',methods=['GET'])
 def home():
@@ -39,7 +43,27 @@ def home():
 @app.route('/graph')
 def graph():
 
-    p = figure(title='Stock prices displayed  for %s' %)
+
+    stock = input() = request.form()
+
+    stock_df = yf.download(stock, start='2020-01-01',end='2020-05-22')
+
+    stock_df =stock_df.reset_index(level=['Date'])
+
+
+    p = figure(title='Stock prices displayed' % stock ,x_axis_label='date',x_axis_type='datetime')
+
+    if request.form.get('Close'):
+        
+        p.line(x=stock_df['Date'].values,y=stock_df['Close'].values,line_width=2,legend="Close")
+
+
+    script, div = components(p)
+
+    return render_template('graph.html',script=script,div=div)
+
+
+
 
 
 
