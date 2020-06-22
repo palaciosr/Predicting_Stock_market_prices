130b4e301b0d341da5147456f74646ee3929d614
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-22 15:18:41 -0700

    making a dataframe for clear output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Stock_predictions.predition_random_prices import all_pred 
 
 from Stock_predictions.ml_sp500 import StockData
+import pandas as pd 
 
 #multi-threading:
 #https://towardsdatascience.com/multithreading-in-python-for-finance-fc1425664e74
@@ -10,13 +11,25 @@ stock_data= StockData()
 date=stock_data.get_pred_dates()
 
 print("dates and prices from the deep learning model")
-print(date)
-print()
+# print(date)
+# print()
 #below will give the future prices and dates 
 
 
-list_prices=stock_data.predict_prices()
-print(list_prices)
+deep_learning_prices=stock_data.predict_prices()
+#numpy array
+list_prices = pd.Series(deep_learning_prices)
+# print(deep_learning_prices)
+
+random_last_6mo,random_last_2wks,date,actual_price=all_pred()
+
+#pd.DataFrame({'email':sf.index, 'list':sf.values})
+
+all_prices = pd.DataFrame({'Random_last_6_months':random_last_6mo,'Actual_prices':actual_price})
+
+print(all_prices)
 
 
-all_pred()
+# all_pred()
+
+#create dataframe to show different scenarios 
