e3f40e1b5c75d54c014c9d14971a48722a21cadf
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-24 11:11:16 -0700

    modularized the main program for price predictions

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 from Stock_predictions.predition_random_prices import all_pred 
-
 from Stock_predictions.ml_sp500 import StockData
 import pandas as pd 
 
 #multi-threading:
 #https://towardsdatascience.com/multithreading-in-python-for-finance-fc1425664e74
 
-stock_data= StockData()
-
-date=stock_data.get_pred_dates()
 
 
-deep_learning_prices=stock_data.predict_prices()
-#numpy array
-deep_learning_prices = pd.Series(deep_learning_prices)
-# print(deep_learning_prices)
+"""
 
-random_last_6mo,random_last_2wks,date,actual_price=all_pred()
-  
-all_prices = pd.DataFrame({'Date':date,'Random_last_2wks':random_last_2wks,
-                            'Random_last_6_months':random_last_6mo,'Deep_learning_prices':deep_learning_prices,
-                            'Actual_prices':actual_price})
+"""
+class PredictionsAggregated:
 
-print(all_prices.head(4))
+    def __init__(self):
+        
+        self.stock_data = StockData()
+        # self.date = self.stock_data.get_pred_dates()
+        self.deep_learning_prices = self.stock_data.predict_prices()
+        self.random_last_6mo,self.random_last_2wks,self.date,self.actual_price = all_pred()
 
 
-# all_pred()
+    def get_price_predictions(self):
 
-#create dataframe to show different scenarios 
+        self.deep_learning_prices = pd.Series(self.deep_learning_prices)
+
+        all_prices = pd.DataFrame({'Date':self.date,'Random_last_2wks':self.random_last_2wks,
+                            'Random_last_6_months':self.random_last_6mo,'Deep_learning_prices_2months':self.deep_learning_prices,
+                            'Actual_prices':self.actual_price})
+
+        return all_prices
+
+pred_prices = PredictionsAggregated().get_price_predictions()
+
+print(pred_prices.head(4))
