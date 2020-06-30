091ae6f696614b33cfd2b344774279957b11b1c3
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-06-30 16:33:39 -0700

    random stock preditions from user choice of stock. Need to fix actual price method broken.

--- Stock_predictions/predition_random_prices.py
+++ Stock_predictions/predition_random_prices.py
@@ -1,68 +1,82 @@
 import random
 import pandas_datareader as web
-import pandas as pd 
+import pandas as pd
+
 
 #predict for the next five days 
 #week of June 8, 2020
 #this comes from the last 6 months 
-def last_six_months(min_price_6_months,max_price_6_months):
+class RandomGeneratePrices:
 
-    six_month_price_data = []
+    def __init__(self,stock):
+        
+        # self.stock = ChooseStock().get_stock()
 
-    for sp500_price in range(5):
-
-        six_month_price_data.append(random.randrange(int(min_price_6_months), int(max_price_6_months)))
-    
-    series_six_month_price_data = pd.Series(six_month_price_data)
-
-    return series_six_month_price_data
-
-def last_2wks(min_price_2wks,max_price_2wks):
-
-    last_2wk_data = []
-
-    for sp500_price in range(5):
-       
-        last_2wk_data.append(random.randrange(int(min_price_2wks), int(max_price_2wks)))
-
-    series_last_2wk_data = pd.Series(last_2wk_data)
-
-    return series_last_2wk_data
-
-def actual_prices():
-
-    data = web.DataReader('^GSPC',data_source='yahoo',start='2020-06-08',end='2020-06-12')
-
-    data=data.reset_index(level=['Date'])
-
-    return data['Date'],data['Open']
-
-def all_pred():
-
+        self.actual_prices = web.DataReader(stock,data_source='yahoo',start='2020-06-08',end='2020-06-12')
     #we get a data with date and prices open close adjusted volume 
-    data_6mo = web.DataReader('^GSPC',data_source='yahoo',start='2020-01-05',end='2020-06-05')
+        self.data_6mo = web.DataReader(stock,data_source='yahoo',start='2020-01-05',end='2020-06-05')
 
-    min_price_6_months = data_6mo['Open'].min()
-    max_price_6_months = data_6mo['Open'].max()
+        self.data_2wks = web.DataReader(stock,data_source='yahoo',start='2020-05-03',end='2020-06-05')
 
-    #lets get random prices for the next 5 days given the last 6 months
-    # print("Prices given the last 6 months randomly")
-    x=last_six_months(min_price_6_months,max_price_6_months)
-    print()
+        self.stock = stock
 
-    data_2wks = web.DataReader('^GSPC',data_source='yahoo',start='2020-05-03',end='2020-06-05')
+        
+    def last_six_months(self,min_price_6_months,max_price_6_months):
 
-    min_price_2wks = data_2wks['Open'].min()
-    max_price_2wks = data_2wks['Open'].max()
+        six_month_price_data = []
 
-    # print("Prices given the last 2 weeks randomly")
-    y=last_2wks(min_price_2wks,max_price_2wks)
-    print()
+        for sp500_price in range(5):
 
-    # print("The actual prices for the prices for the week of June 8, 2020 ")
-    date,open_price=actual_prices()
+            six_month_price_data.append(random.randrange(int(min_price_6_months), int(max_price_6_months)))
+        
+        series_six_month_price_data = pd.Series(six_month_price_data)
 
-    return x,y,date,open_price
+        return series_six_month_price_data
+
+    def last_2wks(self,min_price_2wks,max_price_2wks):
+
+        last_2wk_data = []
+
+        for sp500_price in range(5):
+        
+            last_2wk_data.append(random.randrange(int(min_price_2wks), int(max_price_2wks)))
+
+        series_last_2wk_data = pd.Series(last_2wk_data)
+
+        return series_last_2wk_data
+
+    def actual_prices(self):
+        #'^GSPC'
+
+        # data = web.DataReader(self.stock,data_source='yahoo',start='2020-06-08',end='2020-06-12')
+
+        self.actual_prices=self.actual_prices.reset_index(level=['Date'])
+
+        return self.actual_prices['Date'],self.actual_prices['Open']
+
+    def all_pred(self):
+
+    
+        min_price_6_months = self.data_6mo['Open'].min()
+        max_price_6_months = self.data_6mo['Open'].max()
+
+        #lets get random prices for the next 5 days given the last 6 months
+        # print("Prices given the last 6 months randomly")
+        x=RandomGeneratePrices(self.stock).last_six_months(min_price_6_months,max_price_6_months)
+        print()
+
+        min_price_2wks = self.data_2wks['Open'].min()
+        max_price_2wks = self.data_2wks['Open'].max()
+
+        # print("Prices given the last 2 weeks randomly")
+        y=RandomGeneratePrices(self.stock).last_2wks(min_price_2wks,max_price_2wks)
+        print()
+
+        # print("The actual prices for the prices for the week of June 8, 2020 ")
+        # date,open_price=RandomGeneratePrices(self.stock).actual_prices()
+
+        return x,y,
+        #date,open_price
 
 # x,y,date,open_price=all_pred()
 # print(x,y,date,open_price)
