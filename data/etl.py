6cb951b5c9f8d85435bd4800c446de5b1ab0093f
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-07-23 18:14:36 -0700

    adding ETL process for getting stock data

--- data/etl.py
+++ data/etl.py
@@ -1,10 +1,16 @@
 from sqlalchemy import Column, ForeignKey, Float, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+import sys
+from os import listdir 
+from sqlalchemy.orm import sessionmaker
+
+from yahoo_finance_data import StockData
 
 Base = declarative_base()
 
-# open high low close adj close volume    symbol
+# columns open high low close adj close volume    symbol
 class StockData(Base):
 
     __tablename__ = 'stock_info'
@@ -19,5 +25,13 @@ class StockData(Base):
     volume = Column(Integer,unique=True)
 
 
+engine = create_engine('sqlite:///:memory:', echo = True)
 
+# file_name = 'new_stock_data/' + listdir('new_stock_data')[0]
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind = engine)
+
+session = Session()
 
