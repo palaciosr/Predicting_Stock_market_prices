7ece0fe0deb0a42d96c7b5343e649844110f86f7
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-07-29 09:31:41 -0700

    inputting simple test to check dataframe

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -1,13 +1,9 @@
 import yfinance as yf 
 import pandas as pd 
 import os ,sys
-from main import ChooseStock
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-stock = ChooseStock().get_stock()
-
-# stock = yf.download("FB",data_source='yahoo',start='2020-07-01',end='2020-07-12')
+stock = yf.download("FB",data_source='yahoo',start='2020-07-01',end='2020-07-12')
 # print(type(stock))
 
-assert type(stock) == 'DataFrame'
+assert isinstance(stock,pd.DataFrame)
