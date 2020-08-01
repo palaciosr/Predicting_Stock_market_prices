241c1976140ce0cd2d9056f1b183f664f745cd92
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-07-31 17:43:18 -0700

    checking if ticker exists

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -2,8 +2,19 @@ import yfinance as yf
 import pandas as pd 
 import os ,sys
 
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+
+from Stock_predictions.ml_sp500 import StockData
+
 
 stock = yf.download("FB",data_source='yahoo',start='2020-07-01',end='2020-07-12')
 # print(type(stock))
 
 assert isinstance(stock,pd.DataFrame)
+
+
+
+data = StockData.check_stock_exisits()
+
+assert data == True
