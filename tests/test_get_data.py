6669d462d6018e0e5aee83d28aad042cb4e7653a
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-09-11 10:28:08 -0700

    article for testing in Flask

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -5,19 +5,11 @@ import os ,sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 
-from Stock_predictions.ml_sp500 import StockData
-
-
 stock = yf.download("FB",data_source='yahoo',start='2020-07-01',end='2020-07-12')
-# print(type(stock))
 
 assert isinstance(stock,pd.DataFrame)
 
 
-
-data = StockData.check_stock_exisits()
-
-assert data == True
 
 #test for flask 
 #https://www.patricksoftwareblog.com/unit-testing-a-flask-application/
